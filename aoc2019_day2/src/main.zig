d125651354e4b07c185675cff5f3110cf6344cea
Steven Branson <steve@diodeware.com>
2019-12-02 00:33:52 -0600

    day 2 part 2

--- aoc2019_day2/src/main.zig
+++ aoc2019_day2/src/main.zig
@@ -4,36 +4,42 @@ pub fn main() anyerror!void {
     const allocator = std.heap.direct_allocator;
     const file_string = try std.io.readFileAlloc(allocator, "input.txt");
 
-    var iterator = std.mem.tokenize(file_string, ",");
-    var data = [_]u32{0} ** 256;
-    var i: u32 = 0;
-    while (iterator.next()) |token| {
-        data[i] = std.fmt.parseUnsigned(u32, token, 10) catch 0;
-        i += 1;
-    }
-
-    // "before running the program, replace position 1 with the value 12 and replace position 2 with the value 2"
-    // not sure what this means, I guess can't be fucked to provide the correct puzzle input?????????????????????
-    data[1] = 12;
-    data[2] = 2;
-
-    var opcode_position: u32 = 0;
-    while(true) {
-        switch(data[opcode_position]) {
-            1 => operation_one(&data, opcode_position),
-            2 => operation_two(&data, opcode_position),
-            99 => break,
-            else => unreachable,
+    var noun: u32 = 0;
+    var verb: u32 = 0;
+    while (true) {
+        var iterator = std.mem.tokenize(file_string, ",");
+        var data = [_]u32{0} ** 256;
+        var i: u32 = 0;
+        while (iterator.next()) |token| {
+            data[i] = std.fmt.parseUnsigned(u32, token, 10) catch 0;
+            i += 1;
         }
-        opcode_position += 4;
-    }
-    
-    const stdout = &std.io.getStdOut().outStream().stream;
-    try stdout.print("Result: ");
 
-    var j: u32 = 0;
-    while (j < i) : (j += 1) {
-        try stdout.print("{},", data[j]);
+        data[1] = noun;
+        data[2] = verb;
+
+        var opcode_position: u32 = 0;
+        while(true) {
+            switch(data[opcode_position]) {
+                1 => operation_one(&data, opcode_position),
+                2 => operation_two(&data, opcode_position),
+                99 => break,
+                else => unreachable,
+            }
+            opcode_position += 4;
+        }
+
+        if (data[0] == 19690720) {
+            std.debug.warn("noun: {}, verb: {}", noun, verb);
+            break;
+        } else if (noun < 99) {
+            noun += 1;
+        } else if (verb < 99) {
+            noun = 0;
+            verb += 1;
+        } else {
+            std.debug.warn("Never found it");
+        }
     }
 }
 
