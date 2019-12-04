0424590499ae72ba32f1338ee28a263043b3048e
Steven Branson <steve@diodeware.com>
2019-12-03 23:46:13 -0600

    day 4 part 2

--- aoc2019_day4/src/main.zig
+++ aoc2019_day4/src/main.zig
@@ -13,7 +13,9 @@ pub fn main() anyerror!void {
 
         var failed = false;
         var foundDouble = false;
-        
+        var foundDigit: u8 = 0;
+        var ignore: u8 = 0;
+
         var j: u32 = 1;
         while (j < string.len) : (j += 1) {
             // test ascending
@@ -23,10 +25,21 @@ pub fn main() anyerror!void {
             }
 
             // test double
-            if (string[j] == string[j - 1]) {
-                foundDouble = true;
+            if (string[j] == string[j - 1] and string[j] != ignore) {
+                if (foundDouble) {
+                    // we already have a double
+                    if (foundDigit == string[j]) { 
+                        // but our double is part of a larger group
+                        foundDouble = false;
+                        ignore = string[j];
+                    }
+                } else {
+                    foundDouble = true;
+                    foundDigit = string[j];
+                }
             }
         }
+        
 
         if (failed or !foundDouble) continue;
 
